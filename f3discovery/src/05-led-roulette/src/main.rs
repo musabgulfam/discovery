6781dcf8f3d02ee7821e741b46d33ad8b268ece2
Musab Gulfam <musabgulfam0722@gmail.com>
2022-06-09 01:42:47 +0500

    First challenge completed

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -2,14 +2,25 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use volatile::Volatile;
+use aux5::{Delay, DelayMs, LedArray, OutputSwitch, entry};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let mut half_period = 100_u16;
+    let v_half_period = Volatile::new(&mut half_period);
+
+    loop {
+        for elem in 0..8 {
+            leds[elem].on().ok();
+            delay.delay_ms(v_half_period.read());
+        }
+        for elem2 in 0..8 {
+            leds[elem2].off().ok();
+            delay.delay_ms(v_half_period.read());
+        }
+    }
 }
+
