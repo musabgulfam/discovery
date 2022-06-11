1a90df75a46ebc1129514162d37c5541de212eb6
Musab Gulfam <musabgulfam0722@gmail.com>
2022-06-12 03:43:42 +0500

    Hello world itm

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -10,16 +10,26 @@ fn main() -> ! {
     let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
     let mut half_period = 100_u16;
+    let ms = 50_u8;
     let v_half_period = Volatile::new(&mut half_period);
 
     loop {
-        for elem in 0..8 {
-            leds[elem].on().ok();
-            delay.delay_ms(v_half_period.read());
-        }
-        for elem2 in 0..8 {
-            leds[elem2].off().ok();
-            delay.delay_ms(v_half_period.read());
+        // for elem in 0..8 {
+        //     leds[elem].on().ok();
+        //     delay.delay_ms(v_half_period.read());
+        // }
+        // for elem2 in 0..8 {
+        //     leds[elem2].off().ok();
+        //     delay.delay_ms(v_half_period.read());
+        // }
+        for curr in 0..8 {
+            let next = (curr + 1) % 8;
+
+            leds[next].on().ok();
+            delay.delay_ms(ms);
+
+            leds[curr].off().ok();
+            delay.delay_ms(ms);
         }
     }
 }
