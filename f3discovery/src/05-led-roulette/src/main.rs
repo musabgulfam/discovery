9dd5908b72d5594988e48d573242cce21343b730
Musab Gulfam <musabgulfam0722@gmail.com>
2023-09-13 22:27:10 +0500

    Modification during learning

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -2,14 +2,20 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{Delay, DelayMs, LedArray, OutputSwitch, entry};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let ms = 50_u8;
+    loop {
+        for current in 0..8{
+            let next = (current + 1) % 8;
+            leds[next].on().ok();
+            delay.delay_ms(ms);
+            leds[current].off().ok();
+            delay.delay_ms(ms);
+        }
+    }
 }
